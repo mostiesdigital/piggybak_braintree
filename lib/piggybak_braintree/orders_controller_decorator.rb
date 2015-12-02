0d057b6ffd4d233e1adb6d7e5f170e9488a63e64
Toms Strubergs <toms.strubergs@kodex.lv>
2015-12-02 13:59:27 +0200

    Debug order

--- lib/piggybak_braintree/orders_controller_decorator.rb
+++ lib/piggybak_braintree/orders_controller_decorator.rb
@@ -66,7 +66,8 @@ module PiggybakBraintree
           @order = Piggybak::Order.new
           @order.create_payment_shipment
           @order.initialize_user(current_user)
-          payment_method = ::PiggybakBraintree::PaymentCalculator::Braintree.new(@order.payment_method)
+          binding.pry
+          payment_method = ::PiggybakBraintree::PaymentCalculator::Braintree.new(@order.payments.payment_method)
           payment_method.configure
           @client_token = payment_method.client_token
         end
