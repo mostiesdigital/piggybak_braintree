30c2aff13dedc0f821d77b6afbe4000aa3bfa853
Toms Strubergs <toms.strubergs@kodex.lv>
2015-12-02 16:15:02 +0200

    Fix payments decorator

--- lib/piggybak_braintree/payment_decorator.rb
+++ lib/piggybak_braintree/payment_decorator.rb
@@ -6,20 +6,15 @@ module PiggybakBraintree
       attr_accessor :payment_method_nonce
 
       validates_presence_of :payment_method_nonce, :on => :create
-
-      def credit_card
-        self.payment_method_nonce
-      end
-
+      
       def process(order)
         return true unless self.new_record?
 
         ::PiggybakBraintree::PaymentCalculator::Braintree.new(self.payment_method).configure
 
-        binding.pry
         result = Braintree::Transaction.sale(
             :amount => (order.total_due * 100).to_i,
-            :payment_method_nonce => self.credit_card
+            :payment_method_nonce => self.payment_method_nonce
         )
       end
     end
